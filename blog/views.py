645ff29ec1d4424107d14807df8cb971296155dd
Hyunjoo <hyunjoopark0208@gmail.com>
2024-07-11 20:20:47 +0200

    feat: Create deleting feature

--- blog/views.py
+++ blog/views.py
@@ -40,12 +40,14 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
-def publish(post):
-    post.published_date = timezone.now()
-    post.save()
-
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method=='POST':
         post.publish()
     return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.delete()
+    return redirect('post_list')
